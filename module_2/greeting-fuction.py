25ccb754ffde6d1576d26add10c2aaa2f0102538
William Gonzalez <gonzalez.williamalexander1@gmail.com>
2024-08-02 16:22:22 -0400

    functions

---
+++ module_2/greeting-fuction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def greeting(name,department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+greeting("william", "Cloud Support Operations")
