06c74dbc02fa61adfad329057169c2fc4657fd0c
William Gonzalez <gonzalez.williamalexander1@gmail.com>
2024-08-04 11:18:38 -0400

    dns latency function

--- module_2/dns_latency.py
+++ module_2/dns_latency.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import subprocess
-# simple function ping three dns servers. Then calculate latency, finally prints latency.
+import re
 
-def dns_latency(dns1,dns2,dns3):
-    proc1 = subprocess.check_output(f"ping -c10 {dns1}", shell='true', capture_output=True)
-    proc2 = subprocess.check_output(f"ping -c10 {dns2}", shell='true', capture_output=True)
-    proc3 = subprocess.check_output(f"ping -c10 {dns3}", shell='true', capture_output=True)
-        
-dns_latency("8.8.8.8", "1.1.1.1", "1.1.1.2")
+# Function to ping three DNS servers, calculate latency, and print it
+def dns_latency(dns1, dns2, dns3, dns4):
+    dns_list = [dns1, dns2, dns3, dns4]
+    
+    for dns in dns_list:
+        result = subprocess.run(f"ping -c 3 {dns}", shell=True)
+        print()
+        print()
+
+dns_latency("8.8.8.8", "1.1.1.1", "1.1.1.2", "9.9.9.9")
